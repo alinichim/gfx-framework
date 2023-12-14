917e74d45fb2142a026e213c89aa35f8272db582
Alin Ichim <alinichim49@gmail.com>
2023-12-14 02:46:07 +0200

    refactored

--- src/lab_m1/lab_list.h
+++ src/lab_m1/lab_list.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "lab_m1/Homework1/Homework1.h"
-#include "lab_m1/Homework2/Homework2.h"
 #include "lab_m1/Homework1/GameObject.h"
 #include "lab_m1/Homework1/ActiveGameObject.h"
 #include "lab_m1/Homework1/PassiveGameObject.h"
@@ -14,12 +13,14 @@
 #include "lab_m1/Homework1/Attacker.h"
 #include "lab_m1/Homework1/Defender.h"
 #include "lab_m1/Homework1/Projectile.h"
+#include "lab_m1/Homework2/Homework2.h"
+#include "lab_m1/Homework2/WOTGameObject.h"
 #include "lab_m1/lab1/lab1.h"
 #include "lab_m1/lab2/lab2.h"
 #include "lab_m1/lab3/lab3.h"
 #include "lab_m1/lab3/lab3_vis2D.h"
 #include "lab_m1/lab4/lab4.h"
-#include "lab_m1/lab5/lab5.h"
+//#include "lab_m1/lab5/lab5.h"
 #include "lab_m1/lab6/lab6.h"
 #include "lab_m1/lab7/lab7.h"
 #include "lab_m1/lab8/lab8.h"
